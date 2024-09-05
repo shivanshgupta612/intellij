3fd854e94fc1add29de7dbd23345472d166502e1
shivanshgupta612 <iit2019107@iiita.ac.in>
2024-09-05 21:43:55 +0530

    fb sql and zepto HM round

---
+++ zepto.java
@@ -0,0 +1,16 @@
+// develop a biolerplate for a microservice with basic funtionalities
+// basic functionalities :
+// redis cache fast retrieval ,
+// mysql db
+// column isvailable = true/false;
+// elk stack kibana logstach and elastic search index those loggers
+// maven
+// kubernetes
+// co pilot / chatgpt
+// grafana set alerts mail / slack notification
+// postman / swagger
+// paytm seller panel boilerplate template of message dynamic variables
+// kafka
+// frontend , qa (kukumber)
+public class zepto {
+}
